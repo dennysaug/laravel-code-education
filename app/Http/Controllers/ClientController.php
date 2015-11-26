fc9af09f6da77d22626cb9bc051d74840be49dfe
Dennys Augustus <dennysaug@gmail.com>
2015-11-25 21:52:46 -0300

    apresentar um CRUD completo de nosso model Client.

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Http\Requests;
+use CodeProject\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Client::create($request->except('_token'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Client::find($id)->delete();
+    }
+}
